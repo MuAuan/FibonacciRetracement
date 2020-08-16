eb4e9aa2a55d91f946f46a2c52fae145edbdb9f3
MuAuan <35788955+MuAuan@users.noreply.github.com>
2020-08-16 21:28:25 +0900

    Add files via upload

--- fibonacci.py
+++ fibonacci.py
@@ -4,13 +4,24 @@ def calc_fib(n):
     else:
         return calc_fib(n-1) + calc_fib(n-2)
 
-print('__name__1:', __name__)
+def calc_fib3(m): #fastest
+    a, b = 1, 1
+    for _ in range(m-2):
+        a, b = b, a + b
+    return b    
 
-if __name__ == '__main__':
+def main():
     n0=30    
     for n in range(1,n0+1):
-        print(calc_fib(n),end=' ')
+        print(calc_fib3(n),end=' ')
     print()    
     for n in range(1,n0+1):
-        print('{0:.2f}'.format(calc_fib(n)/calc_fib(n0)*100),end=' ')
+        print('{0:.30f}'.format(calc_fib3(n)/calc_fib3(n0)*100),end=' ')
+    print()    
+    for n in range(1,n0+1):
+        print('{0:.30f}'.format(calc_fib3(n)/calc_fib3(n0-5)*100),end=' ')    
+    
+
+if __name__ == '__main__':
+    main()
 
